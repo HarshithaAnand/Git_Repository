5fcd715f90be09c8a424c6d866d7da847499c26d
Harshi <harshithaanand25@gmail.com>
2023-04-05 18:43:34 +0530

    Adding function3

--- src/com/samplepackage/Calculator.java
+++ src/com/samplepackage/Calculator.java
@@ -49,6 +49,14 @@ public class Calculator {
 						System.out.println("Cannot divide by zero!");
     
 			    }
+					public int  function3(int num1, int num2) {
+				        if (num2 == 0) {
+				            System.out.println("Cannot divide by zero!"); 
+				        }
+				        else {
+				        int result =  num1 / num2;
+				        System.out.println("Division of two numbers:" +result);
+				        return(result);
 	public static void main(String[] args) {
 		try (// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in)) {
