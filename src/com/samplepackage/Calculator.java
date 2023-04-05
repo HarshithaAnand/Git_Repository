0e662e5611596075eaee04700bc7d47adc95c497
Harshi <harshithaanand25@gmail.com>
2023-04-05 18:28:54 +0530

    Changes to develop branch

--- src/com/samplepackage/Calculator.java
+++ src/com/samplepackage/Calculator.java
@@ -26,6 +26,17 @@ public class Calculator {
 			        return(result);
 			        }
 					return 0; 
+			 public int  function1(int num1, int num2) {
+			        if (num2 == 0) {
+			            System.out.println("Cannot divide by zero!"); 
+			        }
+			        else {
+			        int result =  num1 / num2;
+			        System.out.println("Division of two numbers:" +result);
+			        return(result);
+			        }
+					return 0; 
+    
     
 			    }
 	public static void main(String[] args) {
